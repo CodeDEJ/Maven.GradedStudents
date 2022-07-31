90efecb5228cfdb94d9de78b203f32eef8fbddfc
Dejen <dejen@Zipcoders-MacBook-Pro-88.local>
2022-07-31 13:20:43 -0400

    updating classroom

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,5 +1,5 @@
 package io.zipcoder;
-
+import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -66,4 +66,36 @@ public class Classroom {
         }
     }
 
+    public void getGradeBook() {
+        Student temp = new Student("John", "Doe", new ArrayList<>(Arrays.asList(100.0)));
+        for (int i = 0; i < students.length - 1; i++) {
+            for (int j = 1; j < (students.length- i); j++) {
+                if (students[j - 1].getAverageExamScore() < students[j].getAverageExamScore()) {
+                    temp = students[j - 1];
+                    students[j - 1] = students[j];
+                    students[j] = temp;
+                }
+                String grade = null;
+                double max = students[0].getAverageExamScore();
+                double min = students[-1].getAverageExamScore();
+                double range = max - min;
+                List <Student> arr = new ArrayList<Student>();
+
+                for ( int g = 0; g < students.length - 1; g++) {
+                    if (students[g].getAverageExamScore() <= max || students[g].getAverageExamScore() > max - (range * .1)) {
+                        grade = "A";
+                    } else if (max - (range * .11) > students[g].getAverageExamScore() || students[g].getAverageExamScore() > max - (range * .29)) {
+                        grade = "B";
+                    } else if (max - (range * .30) > students[g].getAverageExamScore() || students[g].getAverageExamScore() > max - (range * .50)) {
+                        grade = "C";
+                    } else if (max - (range * .51) > students[g].getAverageExamScore() || students[g].getAverageExamScore() > max - (range * .89)) {
+                        grade = "D";
+                    } else if (max - (range * .11) > students[g].getAverageExamScore()) {
+                        grade = "F";
+                    }
+                    arr.add(students[g]);
+                }
+            }
+        }
+    }
 }
